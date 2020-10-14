c3b45a321e46778ef8ad38a9cde053025174aa29
Roman Gräf <romangraef@loves.dicksinhisan.us>
2020-10-15 00:58:34 +0200

    stuff

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import com.romangraef.uni.PandocTask
 
 
-tasks.register<PandocTask>("runPandoc") {
+val runPandoc by tasks.creating(PandocTask::class) {
+    description = "Generates all htmls from markdown"
+    group = "build"
     inputDir.set(file("src"))
     outputDir.set(file("$buildDir/dist"))
     extraArgs.set(listOf(
@@ -14,4 +16,25 @@ tasks.register<PandocTask>("runPandoc") {
         "-H",
         file("inject_header.html").absolutePath
     ))
+}
+val copyPdfs by tasks.creating(Copy::class) {
+    description = "Copies all pdfs to the dist"
+    group = "build"
+    from("src")
+    into("$buildDir/dist")
+    include("**/*.pdf")
+}
+val build by tasks.creating {
+    description = "Fully builds build/dist"
+    group = "build"
+}
+
+build.dependsOn(copyPdfs)
+build.dependsOn(runPandoc)
+val clean by tasks.creating{
+    description = "Cleans up all build files"
+    group = "build"
+    doLast {
+        file(buildDir).delete()
+    }
 }
